02b6de6fd38f63c18250f7a575d62825d17a29ed
kenizD <1369811517@qq.com>
2016-02-20 18:33:44 +0800

    change pint

--- News/countNum.py
+++ News/countNum.py
@@ -14,7 +14,7 @@ SZ000001origin.diff是从xsl里直接读取的原messageid。
 如果直接输入python countNum.py 
 则会自动计算NewsLinkText里的所有股票。
 
-按理说，爬去的新闻加上badlink里的应该等于总和。但有时候不相等。原因是因为多线程没有加锁。导致线程共享变量：计数的index有时会被多个线程使用
+按理说，爬到本地的新闻加上badlink里的应该等于总和。但有时候不相等。原因是因为多线程没有加锁。导致线程共享变量：计数的index有时会被多个线程使用
 导致重复。且多线程共同写一个文件时（这里是badlink.txt文件）会有复写。使得badlink里的链接少了。
 如果加了锁可以保证所有股票爬下来都能数量一致。但是速度会是不加锁的速度的三倍。所以最后选择没有加锁（getNewsContent.py）。
 
@@ -89,10 +89,11 @@ def diffNum():
         
         print "origin %s.xsl length:%s"%(stockId,len(message_idList))
         print "program %s.xsl length:%s"%(stockId,len(processList))
-        print "%s already download:%s"%(stockId,len(txtList))
-        print "%s badlink:%s"%(stockId,len(badLinkList))
+        print "     %s already download:%s"%(stockId,len(txtList))
+        print "     %s badlink:%s"%(stockId,len(badLinkList))
         if len(message_idList) == len(processList):
             print u"complete"
+            print " "
         else:
             with open("%s.diff"%stockId,"w") as f:
                 for i in processList:
